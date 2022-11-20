c19b79bfadd1374b29ef46409be4fee1b6b4485d
Nick Mason <nickmason372@yahoo.com>
2022-11-20 12:27:08 -0500

    Created models for User, Thought, and Reaction

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require ('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+           
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            required: true
+        }
+       
+    }
+);
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought; 
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require ('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: '[a-z0-9]+@[a-z]+\.[a-z]{2,3}'
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjecyId,
+                ref: "User"
+            },
+        ],
+    }
+);
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model('user', userSchema);
+module.exports = User; 
