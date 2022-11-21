ab5957dbe0f64352a3aed0d94d94dd03eed310ac
Nick Mason <nickmason372@yahoo.com>
2022-11-20 20:02:22 -0500

    added CRUD functions for User

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,46 @@
 const { User, Thought } = require('../models');
 
 const userController = {
- 
-};
+ getUsers(req, res) {
+    User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err));
+ },
+ getOneUser(req,res) {
+    User.findOne({_id: req.params.userId})
+    .select('-__v')
+    .then((user) =>
+    !user
+    ? res.status(404).json({message: 'No user by that ID'})
+    : res.json(course)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+createUser(req, res) {
+    User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+},
+updateUser(req, res) {
+    User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$set:req.body},
+        {runValidators:true, new: true}
+    )
+    .then((user) =>
+    !user
+    ? res.status(404).json({message: 'No user by that ID'})
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+deleteUser(req, res) {
+    User.findOneAndDelete({_id: req.params.userId})
+    .then((user) =>
+    !user
+    ? res.stastus(404).json({message: 'No user by that ID'})
+    : Thought.deleteMany({_id: {$in: user.thoughts}}))
+}
+}; 
 
 module.exports = userController;
